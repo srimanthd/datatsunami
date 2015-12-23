403dcd4c4f3e88b55da649973aafd951da261ca2
srimanthd <srimanth.duggineni@gmail.com>
2015-12-24 00:23:28 +0530

    Favicon update

--- datatsunami_server/node_modules/d3/src/start.js
+++ datatsunami_server/node_modules/d3/src/start.js
@@ -1,2 +1,2 @@
 !function(){
-  var d3 = {version: "3.5.10"}; // semver
+  var d3 = {version: "3.5.12"}; // semver
---
+++ datatsunami_server/public/js/mongoinputs.js
@@ -0,0 +1,4 @@
+
+
+svgArea = d3.select(".container-fluid").append("svg").attr("width",1000).attr("height",500);
+
