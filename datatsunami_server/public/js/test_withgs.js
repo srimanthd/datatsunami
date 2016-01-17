c53d86f86c43a291bdd9bce54fe6574fa44eaf4f
srimanthd <srimanth.duggineni@gmail.com>
2016-01-17 08:47:16 +0530

    Directory structure changes

--- datatsunami_server/public/js/test_withgs.js
+++ datatsunami_server/public/js/test_withgs.js
@@ -1,19 +1,39 @@
 //Connectiong to socket server
-var socket = io('http://192.168.59.129:4000');
+
+width = window.innerWidth;
+height = window.innerHeight;
+
+var socket = null;
+var started = false;
+
+var socketstart = function(){
+	if(!started){
+	socket = io('http://192.168.59.129:4000');
+	//Registering a listener
+	socket.on('takethis', function(mdata) {
+		if(mdata.length!=0){
+			showUpdates(mdata);
+		}
+	});
+		started = true;
+	}
+}
+
+//width of height of d3tophashtags
+width_d3 = document.getElementById('d3tophashtags').offsetWidth;
 
 //Create svg and group
-var svg = d3.select("body").append("svg").attr("width",1300).attr("height",1600);
-var group = svg.append("g").attr("width",1300).attr("height",1600).attr("transform","translate(50,50)").attr("id","trends");
+var svg = d3.select("#d3tophashtags").append("svg").attr("width",width_d3).attr("height",400);
+var group = svg.append("g").attr("width",width_d3).attr("height",400).attr("transform","translate(50,50)").attr("id","trends");
 
 //Create pack and force layout
-var pack = d3.layout.pack().size([1250,550])
+var pack = d3.layout.pack().size([width_d3,250])
 						.children( function(d){ return d.hashtags } )
 						.value( function(d){ return d.count; });
-var force = d3.layout.force().size([1250,550]);
+var force = d3.layout.force().size([width_d3,250]);
 var nodes = null;
 
 //Define init function
-//var func = function(d) { d.px = 1000*Math.random(); d.py= 1000*Math.random() };
 var func = function(d) { d.px = 1000; d.py= 1000 };
 var color = d3.scale.category20();
 
@@ -21,6 +41,7 @@ var color = d3.scale.category20();
 var thistime = 0;
 var lasttime = 0;
 var data_x= null;
+
 //Define showUpades function
 var showUpdates = function(data){
 
@@ -36,7 +57,6 @@ var showUpdates = function(data){
 	circles = group.selectAll("g").data(nodes).enter().append("g");
 	titles = circles.append("title").text( function(d){
 			if(d.count==-1){
-				console.log("Hi");
 				return "Total hashtags";
 			}
 			else{
@@ -51,9 +71,9 @@ var showUpdates = function(data){
 
 //	nodes.forEach(func);
 
-    for(i=(thistime-newitems);i<thistime;i++){
-		func(nodes[i]);
-	}
+    // for(i=(thistime-newitems);i<thistime;i++){
+		// func(nodes[i]);
+	// }
 
 	force.nodes(nodes);
 	force.charge(0);
@@ -67,7 +87,9 @@ var showUpdates = function(data){
 
 //Define tickFunction
 var tickFunction = function(){
+
 	circles.each(collide(0));
+
 	titles.text( function(d){
 			if(d.count==-1){
 				return "Total hashtags";
@@ -76,22 +98,10 @@ var tickFunction = function(){
 				return d.name+":"+d.count;
 			}
 	});
-	act_circles.transition().duration(100).attr("r", function(d) { if(d.r==125){ return 0;} return d.r; } );
-	act_circles.attr("fill", function(d){ return color(d.count)});
-	circles.transition().duration(100).attr("transform", function(d) { return "translate("+d.x+","+(d.y+300)+")"; } );
+	act_circles.transition().duration(100).attr("r", function(d) { return d.r; } );
+	act_circles.transition().duration(100).attr("fill", function(d){ return color(d.count) } );
+	circles.attr("transform", function(d) { console.log(d); return "translate("+d.x+","+(d.y)+")"; } );
 };
-
-//Registering a listener
-socket.on('takethis', function(mdata) {
-	console.log("Hi");
-	showUpdates(mdata);
-});
-
-
-// setTimeout(function(){
-	// data = { "timestamp":100, "hashtags": [ {"name":"1","count":2500},{"name":"2","count":2500} ] }
-	// showUpdates(data);
-// },1000);
 
 // Resolves collisions between d and all other circles.
 function collide(alpha) {
@@ -99,13 +109,11 @@ function collide(alpha) {
   return function(d) {
     quadtree.visit(function(quad, x1, y1, x2, y2) {
       if (quad.point && (quad.point !== d)) {
-//	    console.log(quad.point);
         var x = d.x - quad.point.x,
             y = d.y - quad.point.y,
             l = Math.sqrt(x * x + y * y),
             r = d.r + quad.point.r;
         if (l < r) {
-//		  console.log(l,r);
           l = (l - r) / l * alpha;
           d.x -= x *= l;
           d.y -= y *= l;
@@ -116,10 +124,4 @@ function collide(alpha) {
 	  return false;
     });
   };
-}
-
-var start_all = function(){
-
-	
-	
 }
